3de1f24dacc16b476cf7de2ddce9b2813d9254bc
Nova_starter <cyvack@outlook.com>
2023-05-26 15:57:28 -0700

    Some more fixes

--- src/main/java/com/cyvack/create_crystal_clear/index/CCBlocks.java
+++ src/main/java/com/cyvack/create_crystal_clear/index/CCBlocks.java
@@ -13,30 +13,30 @@ public class CCBlocks {
 
 	//TODO: make this proper
 	public static final GlassBlockList<GlassCasing>
-			CLEAR_GLASS = new GlassBlockList<>(CasingTypes.NORMAL_CASINGS.names, (casing) -> glassCasing(casing, true)),
-			GLASS_CASINGS = new GlassBlockList<>(CasingTypes.NORMAL_CASINGS.names, (casing) -> glassCasing(casing, false));
+		CLEAR_GLASS = new GlassBlockList<>(CasingTypes.NORMAL_CASINGS.names, (casing) -> glassCasing(casing, true)),
+		GLASS_CASINGS = new GlassBlockList<>(CasingTypes.NORMAL_CASINGS.names, (casing) -> glassCasing(casing, false));
 
 	//Glass Encased Shafts
 	public static GlassBlockList<GlassEncasedShaft>
-			GLASS_ENCASED_SHAFTS = new GlassBlockList<>(CasingTypes.GENERAL_ENCASED.names,
-				(casing) -> glassEncasedShaft(casing, false, (p -> new GlassEncasedShaft(p, () -> GLASS_CASINGS.getCasing(casing))))),
+		GLASS_ENCASED_SHAFTS = new GlassBlockList<>(CasingTypes.GENERAL_ENCASED.names,
+			(casing) -> glassEncasedShaft(casing, false, (p -> new GlassEncasedShaft(p, () -> GLASS_CASINGS.getCasing(casing))))),
 
-			CLEAR_GLASS_ENCASED_SHAFTS = new GlassBlockList<>(CasingTypes.GENERAL_ENCASED.names,
-				(casing) -> glassEncasedShaft(casing, true, (p -> new GlassEncasedShaft(p, () -> CLEAR_GLASS.getCasing(casing)))));
+		CLEAR_GLASS_ENCASED_SHAFTS = new GlassBlockList<>(CasingTypes.GENERAL_ENCASED.names,
+			(casing) -> glassEncasedShaft(casing, true, (p -> new GlassEncasedShaft(p, () -> CLEAR_GLASS.getCasing(casing)))));
 
 	//Glass Encased Cogwheels
 	public static final GlassBlockList<GlassEncasedCogwheel>
-			SMALL_GLASS_ENCASED_COGWHEELS = new GlassBlockList<>(CasingTypes.GENERAL_ENCASED.names,
-				(casing) -> glassEncasedCogwheel(casing, false, false, (p -> new GlassEncasedCogwheel(p, false, () -> GLASS_CASINGS.getCasing(casing))))),
+		SMALL_GLASS_ENCASED_COGWHEELS = new GlassBlockList<>(CasingTypes.GENERAL_ENCASED.names,
+			(casing) -> glassEncasedCogwheel(casing, false, false, (p -> new GlassEncasedCogwheel(p, false, () -> GLASS_CASINGS.getCasing(casing))))),
 
-			SMALL_CLEAR_GLASS_ENCASED_COGWHEELS = new GlassBlockList<>(CasingTypes.GENERAL_ENCASED.names,
-					(casing) -> glassEncasedCogwheel(casing, false, true, (p -> new GlassEncasedCogwheel(p, false, () -> CLEAR_GLASS.getCasing(casing))))),
+		SMALL_CLEAR_GLASS_ENCASED_COGWHEELS = new GlassBlockList<>(CasingTypes.GENERAL_ENCASED.names,
+				(casing) -> glassEncasedCogwheel(casing, false, true, (p -> new GlassEncasedCogwheel(p, false, () -> CLEAR_GLASS.getCasing(casing))))),
 
-			LARGE_GLASS_ENCASED_COGWHEELS = new GlassBlockList<>(CasingTypes.GENERAL_ENCASED.names,
-					(casing) -> glassEncasedCogwheel(casing, true, false, (p -> new GlassEncasedCogwheel(p, true, () -> GLASS_CASINGS.getCasing(casing))))),
+		LARGE_GLASS_ENCASED_COGWHEELS = new GlassBlockList<>(CasingTypes.GENERAL_ENCASED.names,
+				(casing) -> glassEncasedCogwheel(casing, true, false, (p -> new GlassEncasedCogwheel(p, true, () -> GLASS_CASINGS.getCasing(casing))))),
 
-			LARGE_CLEAR_GLASS_ENCASED_COGWHEELS = new GlassBlockList<>(CasingTypes.GENERAL_ENCASED.names,
-					(casing) -> glassEncasedCogwheel(casing, true, true, (p -> new GlassEncasedCogwheel(p, true, () -> CLEAR_GLASS.getCasing(casing)))));
+		LARGE_CLEAR_GLASS_ENCASED_COGWHEELS = new GlassBlockList<>(CasingTypes.GENERAL_ENCASED.names,
+				(casing) -> glassEncasedCogwheel(casing, true, true, (p -> new GlassEncasedCogwheel(p, true, () -> CLEAR_GLASS.getCasing(casing)))));
 
 
 	public static void register() {}
