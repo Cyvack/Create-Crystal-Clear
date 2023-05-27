c7fad9933253611cac40fb77375f762362547e0f
Nova_starter <cyvack@outlook.com>
2023-05-26 21:25:54 -0700

    Stream lined glass block builders, fixed more issues

--- src/main/java/com/cyvack/create_crystal_clear/index/GlassUtil.java
+++ src/main/java/com/cyvack/create_crystal_clear/index/GlassUtil.java
@@ -1,15 +1,14 @@
 package com.cyvack.create_crystal_clear.index;
 
-import com.cyvack.create_crystal_clear.content.blocks.GlassCasing;
-import com.cyvack.create_crystal_clear.content.blocks.GlassEncasedCogwheel;
-import com.cyvack.create_crystal_clear.content.blocks.GlassEncasedShaft;
+import com.simibubi.create.content.kinetics.simpleRelays.encased.EncasedCogwheelBlock;
+import com.simibubi.create.content.kinetics.simpleRelays.encased.EncasedShaftBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class GlassUtil {
 
     public static boolean checkForGlassBlock(BlockState state) {
-        return state.getBlock() instanceof GlassEncasedCogwheel
-                || state.getBlock() instanceof GlassEncasedShaft;
+        return state.getBlock() instanceof EncasedCogwheelBlock
+                || state.getBlock() instanceof EncasedShaftBlock;
     }
 
 
